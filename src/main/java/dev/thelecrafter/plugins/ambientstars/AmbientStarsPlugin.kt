ed9cf8d9e1086f2211e0d5360068571bf623a457
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-12-03 19:31:11 +0100

    + Implemented ShootingStars.kt + Implemented an auto shutdown when the TPS is lower than 16 (will restart once it backs up)

--- src/main/java/dev/thelecrafter/plugins/ambientstars/AmbientStarsPlugin.kt
+++ src/main/java/dev/thelecrafter/plugins/ambientstars/AmbientStarsPlugin.kt
@@ -4,6 +4,7 @@ import dev.thelecrafter.plugins.ambientstars.utils.EventCollector
 import io.papermc.lib.PaperLib
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -18,6 +19,24 @@ class AmbientStarsPlugin : JavaPlugin() {
         getLogger = logger
         PaperLib.suggestPaper(getInstance, Level.ALL)
         EventCollector.addAllEvents()
+        ShootingStars.init()
+        disableOnLag()
+    }
+
+    private fun disableOnLag() {
+        object : BukkitRunnable() {
+            override fun run() {
+                if (server.tps.last() <= 16) {
+                    if (!ShootingStars.task.isCancelled) {
+                        ShootingStars.task.cancel()
+                    }
+                } else {
+                    if (ShootingStars.task.isCancelled) {
+                        ShootingStars.init()
+                    }
+                }
+            }
+        }.runTaskTimer(getInstance, 0, 60 * 20)
     }
 
     override fun onDisable() {
---
+++ src/main/java/dev/thelecrafter/plugins/ambientstars/ShootingStars.kt
@@ -0,0 +1,39 @@
+package dev.thelecrafter.plugins.ambientstars
+
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.Particle
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scheduler.BukkitTask
+import javax.annotation.Nonnull
+import kotlin.random.Random
+
+object ShootingStars {
+
+    lateinit var task: BukkitTask
+
+    fun init() {
+        task = object : BukkitRunnable() {
+            override fun run() {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    if (player.location.world.time in 13000..23000) {
+                        val location: Location = player.location.clone()
+                        location.set(location.x + Random.nextDouble(26.0) - 13.0, location.y + Random.nextDouble(25.0, 35.0), location.z + Random.nextDouble(26.0) - 13.0)
+                        if (location.block.type == Material.AIR) {
+                            shootStar(location)
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(AmbientStarsPlugin.getInstance, 0, 20*45)
+    }
+
+    fun shootStar(@Nonnull location: Location) {
+        val deltaX: Double = Random.nextDouble(8.0) - 4.0
+        val deltaY: Double = Random.nextDouble(2.0)
+        val deltaZ: Double = Random.nextDouble(8.0) - 4.0
+        location.world.spawnParticle(Particle.FIREWORKS_SPARK, location, 0, deltaX, deltaY, deltaZ, Random.nextDouble(0.6), null, true)
+    }
+
+}
